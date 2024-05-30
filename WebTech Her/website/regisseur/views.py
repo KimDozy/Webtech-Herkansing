7558d7334150bfecff5c47aed11e966cbfc8fa46
slendystijn12 <48418241+slendystijn12@users.noreply.github.com>
2024-05-30 23:50:27 +0200

    Add files via upload

--- WebTech Her/website/regisseur/views.py
+++ WebTech Her/website/regisseur/views.py
@@ -8,7 +8,7 @@ from website.regisseur.forms import VoegtoeRegisseur, VerwijderForm
 regisseur_bp = Blueprint('regisseur', __name__, template_folder='templates/regisseur')
 
 @login_required
-@regisseur_bp.route('/toevoegen', methods=['GET', 'POST'])
+@regisseur_bp.route('/regtoevoegen', methods=['GET', 'POST'])
 def toevoegen():
     form = VoegtoeRegisseur()
 
@@ -21,10 +21,10 @@ def toevoegen():
         db.session.commit()
         return redirect(url_for('regisseur.lijst'))
     
-    return render_template('regisseur_toevoegen.html', form=form)
+    return render_template('regisseurs_toevoegen.html', form=form)
 
 @login_required
-@regisseur_bp.route('/verwijderen', methods=['GET', 'POST'])
+@regisseur_bp.route('/regverwijderen', methods=['GET', 'POST'])
 def verwijderen():
     form = VerwijderForm()
 
@@ -39,7 +39,7 @@ def verwijderen():
     
     return render_template('regisseur_verwijderen.html', form=form)
 
-@regisseur_bp.route('/lijst')
+@regisseur_bp.route('/reglijst')
 def lijst():
     regisseurs = Regisseur.query.all()
     return render_template('regisseur_lijst.html', regisseurs=regisseurs)
