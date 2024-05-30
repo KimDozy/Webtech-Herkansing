b36dfdee2c9df1989752b01e5ede396d5cb62979
KimDozy <163117955+KimDozy@users.noreply.github.com>
2024-05-30 20:27:19 +0200

    Add files via upload

--- WebTech Her/website/__init__.py
+++ WebTech Her/website/__init__.py
@@ -34,5 +34,31 @@ app.register_blueprint(acteur_bp)
 app.register_blueprint(film_bp)
 app.register_blueprint(regisseur_bp)
 
+def create_app():
+    app = Flask(__name__)
+
+    # Geheime sleutel voor sessies
+    app.config['SECRET_KEY'] = 'geheimesleutel'
+
+    # Configuratie van de database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'  # Het inloggen wordt gedaan via de 'login' route
+
+    from website.acteur.views import acteur_bp
+    from website.film.views import film_bp
+    from website.regisseur.views import regisseur_bp
+
+    app.register_blueprint(acteur_bp)
+    app.register_blueprint(film_bp)
+    app.register_blueprint(regisseur_bp)
+
+    return app
+
 if __name__ == '__main__':
     app.run(debug=True)
